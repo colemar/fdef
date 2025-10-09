47c09ed6ff0c5b17f947c2a0966feae4b5855faa
Marco Coletti <marco.coletti@telecomitalia.it>
2025-10-09 12:39:23 +0200

    Reduced some indentation.

--- fdef.sh
+++ fdef.sh
@@ -36,40 +36,40 @@ fi
 
 fdef ()
 {
-    if [[ -z "$1" ]]; then
-        echo "Error: Please provide a function name (e.g. fdef myfunction)." 1>&2;
-        return 1;
-    fi;
-    local func_name="$1";
-    local temp_file=$(mktemp --suffix=.sh);
-    if declare -f "$func_name" > /dev/null; then
-        declare -f "$func_name" | sed -E 's/^([[:space:]]+)\1/\1/' > "$temp_file";
-        echo "Editing existing function: '${func_name}'.";
-    else
-        echo -e "${func_name}()\n{\n\n  # Function logic here\n  echo \"Function ${func_name} executed.\"\n\n}" > "$temp_file";
-        echo "Creating new function: '${func_name}'.";
-    fi;
-    local initial_hash _;
-    read initial_hash _ <<< $(md5sum "$temp_file");
-    ${EDITOR:-vi} "$temp_file";
-    local final_hash;
-    read final_hash _ <<< $(md5sum "$temp_file");
-    if [[ "$initial_hash" == "$final_hash" ]]; then
-        echo "No changes detected. Function '${func_name}' was not sourced." 1>&2;
-    else
-        source "$temp_file";
-        local funcname
-        read funcname _ < "$temp_file"
-        echo "Function '${funcname}' successfully sourced (temporarily).";
-        echo "Remember to use 'saf' (declare -f > ~/.bash_functions) to save it permanently.";
-    fi;
-    rm -f "$temp_file"
+  if [[ -z "$1" ]]; then
+    echo "Error: Please provide a function name (e.g. fdef myfunction)." 1>&2;
+    return 1;
+  fi;
+  local func_name="$1";
+  local temp_file=$(mktemp --suffix=.sh);
+  if declare -f "$func_name" > /dev/null; then
+    declare -f "$func_name" | sed -E 's/^([[:space:]]+)\1/\1/' > "$temp_file";
+    echo "Editing existing function: '${func_name}'.";
+  else
+    echo -e "${func_name}()\n{\n\n  # Function logic here\n  echo \"Function ${func_name} executed.\"\n\n}" > "$temp_file";
+    echo "Creating new function: '${func_name}'.";
+  fi;
+  local initial_hash _;
+  read initial_hash _ <<< $(md5sum "$temp_file");
+  ${EDITOR:-vi} "$temp_file";
+  local final_hash;
+  read final_hash _ <<< $(md5sum "$temp_file");
+  if [[ "$initial_hash" == "$final_hash" ]]; then
+    echo "No changes detected. Function '${func_name}' was not sourced." 1>&2;
+  else
+    source "$temp_file";
+    local funcname
+    read funcname _ < "$temp_file"
+    echo "Function '${funcname}' successfully sourced (temporarily).";
+    echo "Remember to use 'saf' (declare -f > ~/.bash_functions) to save it permanently.";
+  fi;
+  rm -f "$temp_file"
 }
 echo "Defined function fdef."
 
 _fdef_completion() {
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    readarray -t COMPREPLY < <(compgen -A function -- "$cur")
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  readarray -t COMPREPLY < <(compgen -A function -- "$cur")
 }
 complete -F _fdef_completion fdef
 echo "Bash completion for fdef enabled."
